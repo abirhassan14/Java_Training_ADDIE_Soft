267bccddc472a9f920499f34e621c06d83339617
USER <USER@DESKTOP-QFUDL99>
2024-10-06 12:49:50 +0600

    Git2

---
+++ Class20/src/JavaNetworkingBasics/Git2.java
@@ -0,0 +1,15 @@
+package JavaNetworkingBasics;
+
+public class Git2 {
+
+	public static void main(String[] args) {
+		int git = 10;
+		int hub = 20;
+		int Github = git+hub;
+		System.out.println(Github);
+		System.out.println(Github*git);
+
+
+	}
+
+}
