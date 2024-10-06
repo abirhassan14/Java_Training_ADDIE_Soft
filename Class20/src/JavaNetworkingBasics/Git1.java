87bc2eb332396513672036da988ba8c15783aa9c
USER <USER@DESKTOP-QFUDL99>
2024-10-06 12:47:54 +0600

    Git Code

---
+++ Class20/src/JavaNetworkingBasics/Git1.java
@@ -0,0 +1,14 @@
+package JavaNetworkingBasics;
+
+public class Git1 {
+
+	public static void main(String[] args) {
+		
+		int git = 10;
+		int hub = 20;
+		int Github = git+hub;
+		System.out.println(Github);
+
+	}
+
+}
